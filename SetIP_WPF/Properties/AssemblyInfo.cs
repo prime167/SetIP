f8a7864272cf7f27274b5099aae762bc5689016c
codest <3722845+prime167@users.noreply.github.com>
2019-09-11 13:45:39 +0800

    .

--- SetIP_WPF/Properties/AssemblyInfo.cs
+++ SetIP_WPF/Properties/AssemblyInfo.cs
@@ -40,16 +40,5 @@ using System.Windows;
                                               //、应用程序或任何主题专用资源字典中找到时使用)
 )]
 
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
